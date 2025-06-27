c795aeb2cbfef5597b6255a2414b2bbe0c70f005
Jonas Götz <jonas@42heilbronn.de>
2025-06-27 23:44:22 +0000

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Analytics } from "@vercel/analytics/next"
 import {
   ClerkProvider,
   SignInButton,
@@ -48,6 +49,7 @@ export default function RootLayout({
             </SignedIn>
           </header>
           {children}
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
