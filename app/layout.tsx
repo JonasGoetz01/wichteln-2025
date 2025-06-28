010fb6a0ef9d102808c1e8665693867de3fb335b
Jonas Götz <jonas@42heilbronn.de>
2025-06-28 08:34:11 +0000

    add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import {
   ClerkProvider,
   SignInButton,
@@ -50,6 +51,7 @@ export default function RootLayout({
           </header>
           {children}
           <Analytics />
+          <SpeedInsights />
         </body>
       </html>
     </ClerkProvider>
