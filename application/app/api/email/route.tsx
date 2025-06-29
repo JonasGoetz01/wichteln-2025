e49065a219e4245b7c487a6da19c18a4df586300
Jonas Götz <jonas@42heilbronn.de>
2025-06-29 20:55:49 +0000

    add ability to sent emails

---
+++ application/app/api/email/route.tsx
@@ -0,0 +1,32 @@
+import { Resend } from 'resend';
+import { NextResponse } from "next/server";
+import { auth, currentUser } from '@clerk/nextjs/server'
+
+const resend = new Resend('re_aExWKojp_Pbyv9kWFCcVdVisZPtU58Dfo');
+
+export async function GET() {
+  await auth.protect()
+  const user = await currentUser()
+  if (!user) {
+    return NextResponse.json({
+      message: 'User not found'
+    }, { status: 404 })
+  }
+  const userEmail = user.emailAddresses[0]?.emailAddress
+  if (userEmail !== 'jonas.goetz01@web.de' && userEmail !== 'jonas.goetz01@gmail.com') {
+    return NextResponse.json({
+      message: 'User not allowed'
+    }, { status: 403 })
+  }
+  
+  resend.emails.send({
+    from: 'info@mail.goetz.sh',
+    to: 'jonas.goetz01@web.de',
+    subject: 'Hello World',
+    html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
+  });
+
+  return NextResponse.json({
+    message: 'Email sent'
+  });
+}
